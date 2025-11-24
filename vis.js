72c27461444f6fcf0f060163e31b5b8f7bbb5a3b
irene <lauirene1000@yahoo.com>
2025-11-24 13:29:40 -0800

    embedded vis 1

--- vis.js
+++ vis.js
@@ -1,12 +1,37 @@
 // Load data from datasets/videogames_wide.csv using d3.csv and then make visualizations
 async function fetchData() {
-    const data = await d3.csv("./dataset/videogames_wide.csv");
+    const data = await d3.csv("./dataset/Most_Streamed_Spotify_Songs_2024.csv");
     return data;
 }
 
 fetchData().then(async (data) => {
 
     // visualization 1
+    const vlSpec = vl
+        .markCircle()
+            .data(data)
+            .transform(
+                // filtering some popular artists
+                {filter: {field: "Artist", oneOf: ["Dua Lipa", "Ariana Grande", "Taylor Swift", "Bruno Mars", "Shawn Mendes", "Justin Bieber", "Rihanna"]}}
+            )
+            .encode(
+                vl.x().fieldT("Release Date"),
+                vl.y().fieldQ("TikTok Views"),
+                vl.size().fieldQ("Spotify Streams"),
+                vl.color().fieldN("Artist"),
+                vl.tooltip(
+                    [
+                        {field: "Track"},
+                        {field: "Artist"},
+                        {field: "Spotify Streams"},
+                        {field: "TikTok Views"}
+                    ]
+                )
+            )
+        .title("Spotify and TikTok Popularity of Popular Pop Artists' Songs")
+        .width(800)
+        .height(500)
+        .toSpec();
 
     // visualization 2
 
@@ -14,9 +39,9 @@ fetchData().then(async (data) => {
 
     // visualization 4
 
-
-
-
+    
+    //render
+    render("#view", vlSpec);
 });
 
 async function render(viewID, spec) {
