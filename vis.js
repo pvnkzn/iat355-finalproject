0a6037e4c83c09ece014ddbb02711420f1d8fe9c
irene <lauirene1000@yahoo.com>
2025-12-08 22:47:28 -0800

    edited vis container

--- vis.js
+++ vis.js
@@ -6,33 +6,6 @@ async function fetchData() {
 
 fetchData().then(async (data) => {
 
-    // visualization 1 OLD
-    // const vlSpec = vl
-    //     .markCircle()
-    //         .data(data)
-    //         .transform(
-    //             // filtering some popular artists
-    //             {filter: {field: "Artist", oneOf: ["Dua Lipa", "Ariana Grande", "Taylor Swift", "Bruno Mars", "Shawn Mendes", "Justin Bieber", "Rihanna"]}}
-    //         )
-    //         .encode(
-    //             vl.x().fieldT("Release Date"),
-    //             vl.y().fieldQ("TikTok Views"),
-    //             vl.size().fieldQ("Spotify Streams"),
-    //             vl.color().fieldN("Artist"),
-    //             vl.tooltip(
-    //                 [
-    //                     {field: "Track"},
-    //                     {field: "Artist"},
-    //                     {field: "Spotify Streams"},
-    //                     {field: "TikTok Views"}
-    //                 ]
-    //             )
-    //         )
-    //     .title("Spotify and TikTok Popularity of Popular Pop Artists' Songs")
-    //     .width(800)
-    //     .height(500)
-    //     .toSpec();
-
     // visualization 1 NEW
     const filteredSpotifySongsTiktokViews = data
         .filter(d => d["TikTok Views"])
@@ -54,43 +27,12 @@ fetchData().then(async (data) => {
             )
         )
         .title("Top 30 most popular Spotify Songs vs TikTok Views, with release date")
-        .width(600)
+        // .width(600)
+        .width("container")
         .height(500)
+        .autosize({ type: "fit", contains: "padding" })
         .toSpec();
         
-
-    // visualization 2 OLD
-    // const filteredSpotify = data
-    //     .filter(d => d["Spotify Streams"])
-    //     .sort((a, b) => d3.descending(+a["Spotify Streams"], +b["Spotify Streams"]))
-    //     .slice(0, 30)
-    //     .map(d => (
-    //         { 
-    //             Track: d["Track"], 
-    //             Artist: d["Artist"], 
-    //             Metric: "Spotify Streams", 
-    //             Value: +d["Spotify Streams"], 
-    //             "Spotify Streams": +d["Spotify Streams"]
-    //         }
-    //     ));
-        
-    // const vlSpec2 = vl
-    //     .markBar()
-    //     .data(filteredSpotify)
-    //     .encode(
-    //         vl.y().fieldN("Track").sort("-x").title("Track Title"),
-    //         vl.x().fieldQ("Value").title("Views / Streams"),
-    //         vl.color().fieldN("Artist").title("Artist"),
-    //         vl.tooltip([
-    //             vl.fieldN("Artist"),
-    //             vl.fieldN("Track"),
-    //             vl.fieldQ("Spotify Streams")
-    //         ])
-    //     )
-    //     .width(800)
-    //     .height(400)
-    //     .title("Top 30 Most Streamed Spotify Songs")
-    //     .toSpec();
 
     // visualization 2 NEW
     const viralSongs = data
@@ -146,42 +88,6 @@ fetchData().then(async (data) => {
         .height(400)
         .title("Top 20 Most Viral Songs on Tiktok Compared with Spotify Streams")
         .toSpec();
-
-    
-
-
-    // visualization 3 OLD
-    // const filteredTiktok = data
-    //     .filter(d => d["TikTok Posts"])
-    //     .sort((a, b) => d3.descending(+a["TikTok Posts"], +b["TikTok Posts"]))
-    //     .slice(0, 30)
-    //     .map(d => (
-    //         { 
-    //             Track: d["Track"], 
-    //             Artist: d["Artist"], 
-    //             Metric: "TikTok Posts", 
-    //             Value: +d["TikTok Posts"], 
-    //             "TikTok Posts": +d["TikTok Posts"]
-    //         }
-    //     ));
-
-    // const vlSpec3 = vl
-    //     .markBar()
-    //     .data(filteredTiktok)
-    //     .encode(
-    //         vl.y().fieldN("Track").sort("-x").title("Track Title"),
-    //         vl.x().fieldQ("Value").title("Posts"),
-    //         vl.color().fieldN("Artist").title("Artist"),
-    //         vl.tooltip([
-    //             vl.fieldN("Artist"),
-    //             vl.fieldN("Track"),
-    //             vl.fieldQ("TikTok Posts")
-    //         ])
-    //     )
-    //     .width(800)
-    //     .height(400)
-    //     .title("Top 30 Most Posted TikTok Songs")
-    //     .toSpec();
 
 
     //visualization 3 NEW
@@ -317,39 +223,6 @@ fetchData().then(async (data) => {
         .title("Artist Comparisons")
         .toSpec();
 
-            
-            
-
-
-
-
-
-    // visualization 4 OLD
-    // const filteredSpotifyTiktok = data
-    //     .filter(d => d["Spotify Streams"])
-    //     .sort((a, b) => d3.descending(+a["Spotify Streams"], +b["Spotify Streams"]))
-    //     .slice(0, 30);
-
-
-    // const vlSpec4 = vl
-    //     .markCircle()
-    //     .data(filteredSpotifyTiktok)
-    //     .encode(
-    //         vl.x().fieldT("Release Date").title("Release Date"),
-    //         vl.y().fieldQ("Spotify Streams"),
-    //         vl.color().fieldN("Track").legend(null),
-    //         vl.size().fieldQ("TikTok Posts"),
-    //         vl.tooltip(
-    //             [
-    //                 vl.fieldN("Track"),
-    //                 vl.fieldN("Artist")
-    //             ]
-    //         )
-    //     )
-    //     .width(600)
-    //     .height(500)
-    //     .title("Top 30 most popular Spotify Songs vs TikTok Posts, with release date")
-    //     .toSpec();
 
     // visualization 4 NEW
 
